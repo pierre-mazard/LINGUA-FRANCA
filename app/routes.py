992226fe474cb646feb714d9bec4f8e9a1ceef46
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-05-21 10:42:29 +0200

    add translatio history

--- app/routes.py
+++ app/routes.py
@@ -8,9 +8,11 @@ def index():  # Setting the index route
     translator = GoogleTranslate() # Creating an instance of the GoogleTranslate class
     translation = '' # Initialization of the translation variable
     text_to_translate = '' # Initialization of the text_to_translate variable
+    translations = [] # Initialization of the translations list
     if request.method == 'POST': # Added condition for POST method
         text_to_translate = request.form['text_to_translate'] # Recovery of the text to translate
         source_language = request.form['source_language'] # Source language recovery
         target_language = request.form['target_language'] # Target language recovery
         translation = translator.translate(text_to_translate, source_language, target_language) # Target language recovery
-    return render_template('index.html', translation=translation, text_to_translate=text_to_translate) # Returning the index.html template with the translation and text_to_translate variables
+        translations.append((text_to_translate, translation)) # Add the translation to the history
+    return render_template('index.html', translation=translation, text_to_translate=text_to_translate, translations=translations) # Returning the index.html template with the translation, text_to_translate and translations variables
